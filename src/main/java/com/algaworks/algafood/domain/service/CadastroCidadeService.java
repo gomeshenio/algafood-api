709730bca46d0e7e9016d0fd893aa89c8a9d35ce
henio gomes <heniogomes6@gmail.com>
2023-05-05 15:37:23 -0300

    Tratamento e modelagem de erros

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -15,21 +15,36 @@ import com.algaworks.algafood.domain.repository.EstadoRepository;
 @Service
 public class CadastroCidadeService {
 
+	private static final String MSG_CIDADE_EM_USO = "Cidade de código %d não pode ser removida, pois está em uso";
+
+	private static final String MSG_CIDADE_NAO_ENCONTRADA = "Não existe um cadastro de cidade com código %d";
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
-
 	@Autowired
 	private EstadoRepository estadoRepository;
+	@Autowired
+	private CadastroEstadoService cadastroEstado;
 
+//	public Cidade salvar(Cidade cidade) {
+//		Long estadoId = cidade.getEstado().getId();
+//		Estado estado = estadoRepository.findById(estadoId)
+//				.orElseThrow(() -> new EntidadeNaoEncontradaException(
+//						String.format("Não existe cadastro de estado com o código %d", estadoId)));
+//
+//		cidade.setEstado(estado);
+//
+//		return cidadeRepository.save(cidade);
+//	}
+	
 	public Cidade salvar(Cidade cidade) {
-		Long estadoId = cidade.getEstado().getId();
-		Estado estado = estadoRepository.findById(estadoId)
-				.orElseThrow(() -> new EntidadeNaoEncontradaException(
-						String.format("Não existe cadastro de estado com o código %d", estadoId)));
+	    Long estadoId = cidade.getEstado().getId();
 
-		cidade.setEstado(estado);
+	    Estado estado = cadastroEstado.buscarOuFalhar(estadoId);
 
-		return cidadeRepository.save(cidade);
+	    cidade.setEstado(estado);
+	    
+	    return cidadeRepository.save(cidade);
 	}
 
 	public void excluir(Long cidadeId) {
@@ -38,12 +53,18 @@ public class CadastroCidadeService {
 
 		} catch (EmptyResultDataAccessException e) {
 			throw new EntidadeNaoEncontradaException(
-					String.format("Não existe um cadastro de cidade com código %d", cidadeId));
+					String.format(MSG_CIDADE_NAO_ENCONTRADA, cidadeId));
 
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-					String.format("Cidade de código %d não pode ser removida, pois está em uso", cidadeId));
+					String.format(MSG_CIDADE_EM_USO, cidadeId));
 		}
 	}
+	
+	public Cidade buscarOuFalhar(Long cidadeId) {
+	    return cidadeRepository.findById(cidadeId)
+	        .orElseThrow(() -> new EntidadeNaoEncontradaException(
+	                String.format(MSG_CIDADE_NAO_ENCONTRADA, cidadeId)));
+	} 
 
 }
--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -12,21 +12,29 @@ import com.algaworks.algafood.domain.repository.RestauranteRepository;
 @Service
 public class CadastroRestauranteService {
 
+	private static final String MSG_RESTAURANTE_NAO_ENCONTRADO = "Não existe cadastro de cozinha com código %d";
+
 	@Autowired
 	private RestauranteRepository restauranteRepository;
-	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+
 	
 	public Restaurante salvar(Restaurante restaurante) {
-		Long cozinhaId = restaurante.getCozinha().getId();
-		
-		Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
-				.orElseThrow(() -> new EntidadeNaoEncontradaException(
-						String.format("Não existe cadastro de cozinha com código %d", cozinhaId)));
-
-		restaurante.setCozinha(cozinha);
-		
-		return restauranteRepository.save(restaurante);
+	    Long cozinhaId = restaurante.getCozinha().getId();
+	    
+	    Cozinha cozinha = cadastroCozinha.buscarOuFalhar(cozinhaId);
+	    
+	    restaurante.setCozinha(cozinha);
+	    
+	    return restauranteRepository.save(restaurante);
+	}
+	
+	public Restaurante buscarOuFalhar(Long restauranteId) {
+	    return restauranteRepository.findById(restauranteId)
+	        .orElseThrow(() -> new EntidadeNaoEncontradaException(
+	                String.format(MSG_RESTAURANTE_NAO_ENCONTRADO, restauranteId)));
 	}
 }
